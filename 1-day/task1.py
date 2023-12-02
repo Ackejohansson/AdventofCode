82eadb96632e7c6f358030e1d3d6a289a67598c6
Axel Johansson <axejoh@student.chalmers.se>
2023-12-02 01:10:24 +0100

    Final changes

--- 1-day/task1.py
+++ 1-day/task1.py
@@ -1,14 +1,14 @@
 with open("data.txt", 'r') as file:
-    data = file.read()
+    data = file.read().split("\n")
 
-def find_number(data):
-    numbers = []
-    for line in data.split("\n"):
-        digits = [char for char in line if char.isdigit()]
-        numbers.append(10*int(digits[0]) + int(digits[-1]))
-    return numbers
+def find_sum(data):
+    sum = 0
+    for line in data:
+        digits = [int(char) for char in line if char.isdigit()]
+        sum += 10*digits[0] + digits[-1]
+    return sum
 
-numbers = find_number(data)
-numbers_sum = sum(numbers)
-print(numbers)
-print(numbers_sum)
+sum = find_sum(data)
+print(sum)
+
+
--- 1-day/task2.py
+++ 1-day/task2.py
@@ -1,7 +1,6 @@
 import re
 
 number_map = {
-    'zero': 0,
     'one': 1,
     'two': 2,
     'three': 3,
@@ -13,21 +12,20 @@ number_map = {
     'nine': 9,
 }
 
-with open('1-day/data.txt', 'r') as file:
+with open('data.txt', 'r') as file:
     data = file.read().split('\n')
 
-
+# ?= for overlapping words
 number_regex = re.compile(r'(?=(one|two|three|four|five|six|seven|eight|nine|[1-9]))')
 
 
 def find_number(line):
-    number_matches = number_regex.findall(line)
-    filtered_matches = [match for match in number_matches if match in number_map or match.isdigit()]
-    numeric_values = [number_map[match] if not match.isdigit() else int(match) for match in filtered_matches]
+    matches = number_regex.findall(line)
+    numeric_values = [number_map[match] if not match.isdigit() else int(match) for match in matches]
     return numeric_values[0]*10+numeric_values[-1]
 
-sum=0
-for line in data:
-    numbers = find_number(line)
-    sum += numbers
-print(sum)
+if '__main__' == __name__:
+    sum = 0
+    for line in data:
+        sum += find_number(line)
+    print(sum)
