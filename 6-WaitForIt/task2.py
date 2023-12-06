2cb63076cde4b39700abb9b3f28b5f1080be8499
Axel Johansson <axejoh@student.chalmers.se>
2023-12-06 10:08:41 +0100

    Redid it with matricies for  more readable and efficient code

--- 6-WaitForIt/task2.py
+++ 6-WaitForIt/task2.py
@@ -1,17 +1,13 @@
 import numpy as np
-from math import prod
 
-time = 41_968_894
+t = 41_968_894
 distance = 214_178_911_271_055
-ans = 0
 
-for hold_time in range(1, time):
-    speed = hold_time
-    travel_time = time - hold_time
-    
-    dist = speed * travel_time
+hold_time = np.arange(1, t)
+speed = hold_time
 
-    if dist > distance:
-        ans += 1
-    
+travel_time = t - hold_time
+dist = speed * travel_time
+
+ans = sum(dist > distance)
 print(ans)
