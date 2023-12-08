3e5861c07f241d527a81d6d4b27035cb6cbdae5a
Axel Johansson <axejoh@student.chalmers.se>
2023-12-08 10:24:22 +0100

    Task 1 complete

--- 7-poker/task1.py
+++ 7-poker/task1.py
@@ -1,13 +1,23 @@
-strength_map = {"J":"A", "Q":"B", "K":"C", "A":"D"}
+strength_map = {"T":"A", "J":"B", "Q":"C", "K":"D", "A":"E"}
 
 def classify(hand):
-    pass
-
+    counts = [hand.count(card) for card in hand]
+    if 5 in counts:
+        return 6
+    if 4 in counts:
+        return 5
+    if 3 in counts:
+        if 2 in counts:
+            return 4
+        return 3
+    if 2 in counts:
+        if counts.count(2) == 4:
+            return 2
+        return 1
+    return 0
+    
 def strength(hand):
-    # Want to return 
-    # the type and 
-    # alpabetical order of the hand based on strength
-    return classify(hand), [strength.get(char, char) for char in hand]
+    return classify(hand), [strength_map.get(char, char) for char in hand]
 
 
 plays = []
@@ -16,3 +26,9 @@ for line in open(0):
     plays.append((hand, int(bet)))
 
 plays.sort(key=lambda play:strength(play[0]))
+
+ans = 0
+for rank, (hand, value) in enumerate(plays, 1):
+    ans += rank * value
+
+print(ans)
