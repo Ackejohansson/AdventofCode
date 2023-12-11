0fe2be74bba17af4e2870f765b2d8a3f05eb4415
Axel Johansson <axejoh@student.chalmers.se>
2023-12-11 13:41:36 +0100

    solution should work but takes forever

--- 8-HauntedWasteland/task2.py
+++ 8-HauntedWasteland/task2.py
@@ -12,11 +12,13 @@ for line in rest:
     pos, target = line.split(" = ")
     network[pos] = target[1:-1].split(", ")
 
-key = "AAA"
+keys = [key for key in network if key.endswith("A")]
 step_count = 0
 
-while key != "ZZZ":
-    key = network[key][0 if steps[0] == "L" else 1]
+
+while not all([key.endswith("Z") for key in keys]):
+    for i, key in enumerate(keys):
+        keys[i] = network[key][0 if steps[0] == "L" else 1]
     
     steps = steps[1:] + steps[0]
     step_count +=1
