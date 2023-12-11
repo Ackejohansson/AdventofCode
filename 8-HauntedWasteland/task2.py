207d7caf7620b4b579a9ec907aa5100f87063442
Axel Johansson <axejoh@student.chalmers.se>
2023-12-11 14:06:12 +0100

    Optimized task 8.2 with ranges and lcm

--- 8-HauntedWasteland/task2.py
+++ 8-HauntedWasteland/task2.py
@@ -1,4 +1,5 @@
 import os
+from math import gcd
 
 file_name = "data.txt"
 dir_path = os.path.dirname(__file__)
@@ -13,14 +14,21 @@ for line in rest:
     network[pos] = target[1:-1].split(", ")
 
 keys = [key for key in network if key.endswith("A")]
-step_count = 0
+
+cycles = []
+for position in keys:
+    step_count = 0
+    while not position.endswith("Z"):
+        position = network[position][0 if steps[0] == "L" else 1]
+        
+        steps = steps[1:] + steps[0]
+        step_count +=1
+    cycles.append(step_count)
+
+result = 1
+for number in cycles:
+    result = number * result // gcd(number, result)
+
+print(result)
 
 
-while not all([key.endswith("Z") for key in keys]):
-    for i, key in enumerate(keys):
-        keys[i] = network[key][0 if steps[0] == "L" else 1]
-    
-    steps = steps[1:] + steps[0]
-    step_count +=1
-
-print(step_count)
