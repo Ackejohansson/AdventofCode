1ccd08af0519b66f681f77dcdb831bbd33395421
Axel Johansson <axel.johansson.2@consultant.volvo.com>
2025-12-01 17:38:23 +0100

    Refactor with task rules for fun

--- scala_solutions/src/main/scala/aoc/day01.scala
+++ scala_solutions/src/main/scala/aoc/day01.scala
@@ -5,56 +5,51 @@ import zio.stream.{ZStream, ZPipeline}
 
 object Day01 extends ZIOAppDefault {
   private val InputFile = "../inputs/day1.txt"
-  // case class InputRow(direction: String, step: Int)
+  private val GridWidth = 100
+  private val StartPos = 50
 
-  val inputStream: ZStream[Any, Throwable, String] =
+  case class Command(direction: Char, step: Int) {
+    def diff: Int = if (direction == 'R') then step else -step
+  }
+
+  def parseCommand(line: String): Command = {
+    Command(line.head, line.drop(1).toInt)
+  }
+
+  val inputStream: ZStream[Any, Throwable, Command] =
     ZStream
       .fromFileName(InputFile)
       .via(ZPipeline.utf8Decode >>> ZPipeline.splitLines)
-      .tap(line => Console.printLine(s"Debug: $line"))
-
-  def part1(
-      stream: ZStream[Any, Throwable, String]
-  ): ZIO[Any, Throwable, Int] = {
-    stream
-      .runFold((50, 0)) { case ((curr, count), line) =>
-        val direction = line.head
-        val step = line.drop(1).toInt
-
-        val diff = if direction == 'R' then step else -step
-        val next = curr + diff
-        val nextShift = Math.floorMod(next, 100)
-        val newCount = if nextShift == 0 then count + 1 else count
-        (nextShift, newCount)
-      }
-      .map { case (curr, count) => count }
+      .map(parseCommand)
+  
+  val part1rule: (Int, Int)=> Int = (curr, next) => {
+    val nextShift = Math.floorMod(next, GridWidth)
+    if (nextShift == 0) then 1 else 0
   }
-  def part2(
-      stream: ZStream[Any, Throwable, String]
-  ): ZIO[Any, Throwable, Long] = {
-      stream
-      .runFold((50, 0)) { case ((curr, count), line) =>
-        val direction = line.head
-        val step = line.drop(1).toInt
 
-        val diff = if direction == 'R' then step else -step
-        val offset = if direction == 'R' then 0 else -1
+  val part2rule: (Int, Int) => Int = (curr, next) => {
+    val offset = if (next > curr) then 0 else -1
 
-        val currMod = Math.floorDiv(curr + offset, 100)
-        val nextMod = Math.floorDiv(curr + diff + offset, 100)
+    val currMod = Math.floorDiv(curr + offset, GridWidth)
+    val nextMod = Math.floorDiv(next + offset, GridWidth)
+    (nextMod - currMod).abs
+  }
 
-        val newCount = (nextMod - currMod).abs
-        
-        (curr + diff, count + newCount)
+  def solve(stream: ZStream[Any, Throwable, Command], rule: (Int, Int) => Int): ZIO[Any, Throwable, Int] = {
+    stream
+      .runFold((StartPos, 0)) { case ((curr, count), cmd) =>
+        val next = curr + cmd.diff
+        val newCount = rule(curr, next)
+        (next, count + newCount)
       }
       .map { case (curr, count) => count }
   }
   override def run: ZIO[Any, Any, Any] = {
     for {
       _ <- Console.printLine("--- DAY ONE START ---")
-      p1 <- part1(inputStream)
+      p1 <- solve(inputStream, part1rule)
       _ <- Console.printLine(s"Part One solution: $p1")
-      p2 <- part2(inputStream)
+      p2 <- solve(inputStream, part2rule)
       _ <- Console.printLine(s"Part Two solution: $p2")
 
     } yield ()
