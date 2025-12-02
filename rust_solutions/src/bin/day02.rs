0a703879e6d37072a56eaead8d53e14e43fa517a
Axel Johansson <axejoh@student.chalmers.se>
2025-12-02 21:41:06 +0100

    Day 2 solution

--- rust_solutions/src/bin/day02.rs
+++ rust_solutions/src/bin/day02.rs
@@ -1,38 +1,70 @@
 use std::fs;
-
-fn read_input() -> String {
-    let input_file = "../inputs/day02_test.csv";
-    fs::read_to_string(&input_file).unwrap()
-}
-
+use std::str::FromStr;
 struct DataRange {
     min: i64,
     max: i64,
 }
-fn parse_chunk(chunk: &str) -> DataRange {
-    let (min, max) = chunk.split_once("-").expect("Bad format: missing dash");
-    let min_i: i64 = min.parse().expect("Expected int");
-    let max_i: i64 = max.parse().expect("Expected int");
-
-    DataRange {
-        min: min_i,
-        max: max_i,
+impl FromStr for DataRange {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (min_str, max_str) = s.split_once("-").ok_or("Bad format: missing dash")?;
+        Ok(DataRange {
+            min: min_str.parse().map_err(|_| "Invalid Min")?,
+            max: max_str.parse().map_err(|_| "Invalid Max")?,
+        })
     }
 }
 
-fn p1(input: Vec<DataRange>) -> i64 {
-    for line in input.lines() {
-        println!("{}\n", line);
-        let pair = line.split(",");
+fn read_input() -> String {
+    fs::read_to_string("../inputs/day02_test.csv")
+        .expect("❌ CRITICAL ERROR: Could not find input file!")
+}
+
+fn filter1(number: i64) -> bool {
+    let nr_str: String = number.to_string();
+    if nr_str.len() % 2 != 0 {
+        return false;
+    };
+    let (left, right) = nr_str.split_at(nr_str.len() / 2);
+    left == right
+}
+
+fn filter2(number: i64) -> bool {
+    let nr_str = number.to_string();
+    let len = nr_str.len();
+
+    for num in 1..=len / 2 {
+        if nr_str.len() % num != 0 {
+            continue;
+        }
+
+        let slice = &nr_str[..num];
+        if slice.repeat(len / num) == nr_str {
+            return true;
+        }
     }
-    10
+    false
+}
+
+fn solution(input: &[DataRange], filter: fn(i64) -> bool) -> i64 {
+    let mut sum: i64 = 0;
+    for dr in input {
+        for number in dr.min..=dr.max {
+            if filter(number) {
+                sum += number;
+            }
+        }
+    }
+    sum
 }
 
 fn main() {
     let input: String = read_input();
-    let ranges: Vec<DataRange> = input.split(",").map(parse_chunk).collect();
+    let ranges: Vec<DataRange> = input.split(",").filter_map(|s| s.parse().ok()).collect();
+
     println!("--- Day 1: Part 1 ---");
-    let result1 = p1(&ranges);
-    println!("1: {}", result1);
-    println!("--- Day 1: Part 2 ---");
+    println!("1: {}", solution(&ranges, filter1));
+
+    println!("\n--- Day 1: Part 2 ---");
+    println!("2: {}", solution(&ranges, filter2));
 }
