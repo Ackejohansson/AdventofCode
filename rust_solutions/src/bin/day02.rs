d8598f0e33d980ea61371ae488ea394d3e0262ff
Axel Johansson <axel.johansson.2@consultant.volvo.com>
2025-12-02 16:00:19 +0100

    Continue of rust impl

--- rust_solutions/src/bin/day02.rs
+++ rust_solutions/src/bin/day02.rs
@@ -4,15 +4,7 @@ fn read_input() -> String {
     let input_file = "../inputs/day02_test.csv";
     fs::read_to_string(&input_file).unwrap()
 }
-fn parse_lines(input: &str) -> String {
-    let lines = input.split(",");
-    for line in lines {
-        let (min, max) = line.split("-");
-        println!("MIN: {}, max: {}", min, max)
-    }
 
-    "10fa".to_string()
-}
 struct DataRange {
     min: i64,
     max: i64,
@@ -28,7 +20,7 @@ fn parse_chunk(chunk: &str) -> DataRange {
     }
 }
 
-fn p1(input: &str) -> i64 {
+fn p1(input: Vec<DataRange>) -> i64 {
     for line in input.lines() {
         println!("{}\n", line);
         let pair = line.split(",");
@@ -38,10 +30,9 @@ fn p1(input: &str) -> i64 {
 
 fn main() {
     let input: String = read_input();
-    let ranges = input.split(",").map(parse_chunk);
-    let parsed = parse_lines(&input);
+    let ranges: Vec<DataRange> = input.split(",").map(parse_chunk).collect();
     println!("--- Day 1: Part 1 ---");
-    let result1 = p1(&input);
+    let result1 = p1(&ranges);
     println!("1: {}", result1);
     println!("--- Day 1: Part 2 ---");
 }
