668c0f4ae3305311f47c832110d147762f82c5d7
Axel Johansson <axel.johansson.2@consultant.volvo.com>
2025-12-02 14:01:28 +0100

    Refactor for cleaner combined solution

--- scala_solutions/src/main/scala/aoc/day02.scala
+++ scala_solutions/src/main/scala/aoc/day02.scala
@@ -3,7 +3,7 @@ package aoc
 import zio.{ZIOAppDefault, ZIO, Console}
 import zio.stream.ZStream
 import zio.stream.ZPipeline
-import zio.stream.ZStreamProvideMacro
+import java.io.InputStream
 
 object Day02 extends ZIOAppDefault {
   private val InputFile = "../inputs/day02.csv"
@@ -15,16 +15,15 @@ object Day02 extends ZIOAppDefault {
     }
   }
 
-  def task1Filter(number: Long): Boolean = {
+  val task1Filter: Long => Boolean = number => {
     val numberStr = number.toString
-
     numberStr.length % 2 == 0 && {
       val (left, right) = numberStr.splitAt(numberStr.length / 2)
       left == right
     }
   }
 
-  def task2Filter(number: Long): Boolean = {
+  val task2Filter: Long => Boolean = (number) => {
     val nrStr = number.toString
     val possibleSplits =
       (1 to nrStr.length / 2).filter(x => nrStr.length % x == 0)
@@ -37,21 +36,35 @@ object Day02 extends ZIOAppDefault {
   val inputStream = ZStream
     .fromFileName(InputFile)
     .via(ZPipeline.utf8Decode >>> ZPipeline.splitLines)
-    .mapConcat(line => line.split(","))
-    .map(parseInput)
-    .flatMap { case (start, end) =>
-      ZStream.iterate(start)(_ + 1L).takeWhile(_ <= end)
-    }
-    .filter(task2Filter)
 
+  def toIds(
+      stream: ZStream[Any, Throwable, String]
+  ): ZStream[Any, Throwable, Long] = {
+    stream
+      .mapConcat(line => line.split(","))
+      .map(parseInput)
+      .flatMap { case (start, end) =>
+        ZStream.iterate(start)(_ + 1L).takeWhile(_ <= end)
+      }
+  }
+
+  def solve(
+      stream: ZStream[Any, Throwable, Long],
+      rule: Long => Boolean
+  ): ZIO[Any, Throwable, Long] = {
+    stream.filter(rule).runSum
+  }
   override def run = {
+    val idStream = toIds(inputStream)
+
     for {
       _ <- Console.printLine("--- DAY 2 Start ---")
-      p1 <- inputStream.filter(task1Filter).runSum
-      _ <- Console.printLine(s"First: $p1")
-      p2 <- inputStream.filter(task2Filter).runSum
-      _ <- Console.printLine(s"Second: $p2")
 
+      p1 <- solve(idStream, task1Filter)
+      _ <- Console.printLine(s"First: $p1")
+
+      p2 <- solve(idStream, task2Filter)
+      _ <- Console.printLine(s"Second: $p2")
     } yield ()
   }
 }
